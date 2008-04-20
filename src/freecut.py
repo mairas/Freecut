c2794b27055decac10c6a37aadc6f63aef85bfb9
mairas <mairas@7b4548a0-3021-0410-bbe8-d2ccb9cc0eb4>
2008-04-20 20:29:26 +0000

    baseline algorithm with no shortcuts
    git-svn-id: file:///home/matairas/o/proj/svnroot/freecut/trunk@23 7b4548a0-3021-0410-bbe8-d2ccb9cc0eb4

--- src/freecut.py
+++ src/freecut.py
@@ -2,197 +2,130 @@
 
 import numpy
 import pylab
+import copy
 
 class Item(object):
-    def __init__(self,w,l):
-        self.w = max(w,l)
-        self.l = min(w,l)
-        self.x = 0
-        self.y = 0
+    def __init__(self,l,w):
+        self.l = max(l,w)
+        self.w = min(l,w)
+        self.x = None
+        self.y = None
 
     def value(self):
         return self.l*self.w
 
     def rotate(self,b):
         if b==False:
-            w = max(self.w,self.l)
-            l = min(self.w,self.l)
-        else:
             l = max(self.w,self.l)
             w = min(self.w,self.l)
+        else:
+            w = max(self.w,self.l)
+            l = min(self.w,self.l)
         self.w = w
         self.l = l 
 
     def rect(self,color,text=None):
         rect(self.x,self.y,self.l,self.w,color,text)
 
+class Region(object):
+    def __init__(self,l,w,x,y):
+        self.l = l
+        self.w = w
+        self.x = x
+        self.y = y
+        
+    def value(self):
+        return self.l*self.w
+        
+    def fill(self,pool,rotated=False):
+        for i in range(len(pool)):
+            pool[i].rotate(rotated)
+            # check if the item actually fits the region
+            if pool[i].w>self.w or pool[i].l>self.l:
+                continue
+            item = pool.pop(i)
+            items = [item]
+            region_A,region_B = self.split(item)
+            items = items + region_A.layout(pool)
+            # if every item is placed, just return
+            if len(pool)==0: return items
+            items = items + region_B.layout(pool)
+            if len(pool)==0: return items
+            pool.insert(i,item)
+            # TODO: need to try filling region_B first
+        # if we have not returned by now, we have failed to fill the region
+        items = []
+        return items
+            
+    def layout(self,pool):
+        # nothing will fit in zero space
+        if self.value()==0:
+            return []
+        # first place the item as-is
+        items = self.fill(pool,rotated=False)
+        if len(pool)!=0:
+            # place the item rotated
+            items_r = self.fill(pool,rotated=True)
+            items = items + items_r
+            
+        return items
+        
+            
+class Block(Region):
+    def split(self,item,rotated=False):
+        xA = self.x+item.l
+        yA = self.y
+        lA = self.l-item.l
+        wA = item.w
+        
+        xB = self.x
+        yB = self.y+item.w
+        lB = self.l
+        wB = self.w-item.w
+        
+        item.x = self.x
+        item.y = self.y
+        
+        return (Block(lA,wA,xA,yA),Block(lB,wB,xB,yB))
+        
+class Segment(Region):
+    def split(self,item,rotated=False):
+        xA = self.x
+        yA = self.y+item.w
+        lA = item.l
+        wA = self.w-item.w
+        
+        xB = self.x+item.l
+        yB = self.y
+        lB = self.l-item.l
+        wB = self.w
+        
+        item.x = self.x
+        item.y = self.y
+        
+        return (Block(lA,wA,xA,yA),Segment(lB,wB,xB,yB))
+
+
 def rect(x,y,l,w,c,text=None):
     pylab.plot([x,x,x+l,x+l,x],[y,y+w,y+w,y,y],c)
     if text!=None:
         pylab.text(x+l/2,y+w/2,text)
 
-def plot_layout(I,B,L,W):
+def plot_layout(items,L,W):
     colors = ['r','g','b','c','m','y']
-    if any(B):
+    if len(items)>0:
         pylab.clf()
         pylab.axes(aspect='equal')
         rect(0,0,L,W,'k')
 
-        for i,item in enumerate(I):
-            if B[i]==True:
-                item.rect(colors[i%len(colors)],str(i))
-        
-    pylab.draw()
+        for i,item in enumerate(items):
+            item.rect(colors[i%len(colors)],str(i))
+        pylab.draw()
     
-def F(x,y):
-    '''Value function.'''
-    return x*y
-
-def LayoutItem(l,w,x,y,Vf,Uf,B,C,kc,I,i,S,vmax,Vs,segment=False):
-    # step 4.1
-    if I[i].l>l or I[i].w>w:
-        return B,C,kc,vmax,Vs
-    if kc==0:
-        kc = i
-
-    # step 4.2
-    I[i].x = x
-    I[i].y = y
-
-    si = I[i].value()
-
-    # after i has been placed, divide remaining L-shaped area into A and B
-    
-    if segment==False:
-        xA = x+I[i].l
-        yA = I[i].y
-        lA = l-I[i].l
-        wA = I[i].w
-        segA = False
-    
-        xB = I[i].x
-        yB = I[i].y+I[i].w
-        lB = l 
-        wB = w-I[i].w
-        segB = False
-    else:
-        xA = I[i].x
-        yA = I[i].y+I[i].w
-        lA = I[i].l
-        wA = w-I[i].w
-        segA = False
-
-        xB = I[i].x+I[i].l
-        yB = 0
-        lB = l-I[i].l
-        wB = w
-        segB = True
-
-    # always fill the smaller region first
-    # FIXME: this evokes a bug!
-    if lB*wB<lA*wA:
-        xA,yA,lA,wA,segA,xB,yB,lB,wB,segB = xB,yB,lB,wB,segB,xA,yA,lA,wA,segA
-        
-    ub = F(l,w)
-    # upper bound for the whole pattern
-    up = Vf + Uf + ub
-
-    if ub<=vmax or up<=Vs:
-        return B,C,kc,vmax,Vs
-
-    # step 4.3
-    
-    B[i] = True
-    CA = numpy.zeros(len(B),bool)
-    VfA = Vf + si
-    UfA = Uf+F(lB,wB)
-    vA,B,CA = Layout(lA,wA,xA,yA,VfA,UfA,B,CA,kc,I,S,vmax,Vs,segA)
-
-    # step 4.4
-    
-    ub = si + vA + F(lB,wB)
-    up = Vf + Uf + ub
-
-    if not (ub<=vmax or up<=Vs):
-        # step 4.5
-        B = B+CA
-        CB = numpy.zeros(len(B),bool)
-        VfB = Vf + si + vA
-        UfB = Uf
-        vB,B,CB = Layout(lB,wB,xB,yB,VfB,UfB,B,CB,kc,I,S,vmax,Vs,segB)
-        v = si + vA + vB
-        if v>vmax:
-            # step 4.6
-            vmax = v
-            C = CA + CB
-            C[i] = True
-            if Vf+vmax>Vs:
-                # step 4.7
-                Vs = Vf+vmax
-                if Vs==S:
-                    return B,C,kc,vmax,Vs
-        # step 4.8
-        B = B-CA
-    # step 4.9
-    B[i] = False
-    return B,C,kc,vmax,Vs
-
-def Layout(l,w,x,y,Vf,Uf,B,C,k,I,S,vmax=0,Vs=0,segment=False):
-    '''
-    Block layout optimizer.
-    
-    Arguments:
-    l    block length (x-wise dim)
-    w    block width (y-wise dim)
-    x    block x location
-    y    block y location
-    Vf   value of items that have already been placed on the plate
-    Uf   the upper bound of the unoccupied regions
-    B    a boolean array of items in the father pattern
-    C    a boolean array of items in the current block
-    k    minimum item index
-    I    list of all items
-    vmax best value of block x y so far
-    Vs   (V*) value of the best pattern obtained so far
-    Returns:
-    value of the found region
-    '''
-    # step 1
-    #pdb.set_trace()
-    if F(l,w)==0:
-        return 0,B,C
-    else:
-        vmax = 0
-        kc = 0
-
-    # steps 2,3,6
-    for i in range(k,len(I)):
-        if B[i]==True: continue  # item already placed
-
-        # step 4
-        I[i].rotate(False)
-        B,C,kc,vmax,Vs = LayoutItem(l,w,x,y,Vf,Uf,B,C,kc,I,i,S,vmax,Vs,segment)
-        if Vs==S:
-            return vmax,B,C
-        # step 4.9
-        if vmax!=l*w and B[i]==False and C[i]==False: # two latter conditions missing from the algorithm description
-            # step 5
-            
-            I[i].rotate(True)
-            B,C,kc,vmax,Vs = LayoutItem(l,w,x,y,Vf,Uf,B,C,kc,I,i,S,vmax,Vs,segment)
-            # if the item was not placed, rotate it back
-            if B[i]==False and C[i]==False:
-                I[i].rotate(False)
-        
-            if Vs==S:
-                return vmax,B,C
-    return vmax,B,C
-
-  
-
 def optimize_HRBB(I,W,alpha):
     # arrange the items in descending order of their areas
     I.sort(reverse=True,key=lambda x: x.w*x.l)
+    N = len(I)
 
     l_max = max([el.l for el in I])
     w_max = max([el.w for el in I])
@@ -207,22 +140,21 @@ def optimize_HRBB(I,W,alpha):
     b = Lmax
     while b-a>1:
         c = (a+b)/2
-        B = numpy.zeros(len(I),bool)
-        C = numpy.zeros(len(I),bool)
         Vs = S-0.1
         print c
-        V,Bl,Cl = Layout(c,W,0,0,0,0,B,C,0,I,S,0,Vs,True)
-        if V<S:
+        r = Segment(c,W,0,0)
+        pool = copy.copy(I)
+        items = r.layout(pool)
+        if len(items)<N:
             a = c
         else:
             b = c
     c = b
-    # get the optimal layout once more
-    B = numpy.zeros(len(I),bool)
-    C = numpy.zeros(len(I),bool)
-    Vs = S-0.1
-    V,Bl,Cl = Layout(c,W,0,0,0,0,B,C,0,I,S,0,Vs,True)
-    plot_layout(I,Bl,c,W)
+    r = Segment(c,W,0,0)
+    items = r.layout(I)
+    
+    if len(items)==N:
+        plot_layout(items,c,W)
 
 if __name__=='__main__':
     I=[
@@ -234,7 +166,7 @@ if __name__=='__main__':
        Item(138,138),
        Item(188,62),
        Item(188,62),
-        Item(650,74),
+#        Item(650,74),
 #        Item(650,74),
 #        Item(650,74),
 #        Item(650,74),
@@ -259,7 +191,7 @@ if __name__=='__main__':
        ]
 
     pylab.ion()
-    optimize_HRBB(I,1830,5.0)
+    optimize_HRBB(I,1830,2.0)
     pylab.show()
     
     for item in I:
