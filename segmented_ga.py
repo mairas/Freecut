79852bf3148457629ddcebe86d3e12dc1e808abc
Matti Airas <matti.p.airas@nokia.com>
2009-03-20 11:26:18 -0300

    basic regions implemented (not tested)

--- segmented_ga.py
+++ segmented_ga.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+# -*- coding: utf-8 -*-
 
 import pygena
 import sys
@@ -10,10 +11,12 @@ class Type(object):
         self.h = h
         self.description = description
 
-class Piece(object):
+class Item(object):
     def __init__(self,type_,rotated):
         self.type = type_
         self.rotated = rotated
+        # location within the region tree
+        self.location = None
 
     def rotate(self):
         self.rotated = not self.rotated
@@ -21,11 +24,58 @@ class Piece(object):
         
 
 class Region(object):
+    """
+    Region is any rectangular area on a plane.
+    A Region may contain an item, in which case the remaining area is divided
+    into two subregions.
+    """
     def __init__(self,l,w):
         self.l = l
         self.w = w
+        self.item = None
+        self.subregions = []
 
     def area(self):
         return self.l*self.w
 
-    
+class Block(Region):
+    """
+    Block is a region divided as follows:
+    +-------+
+    |   B   |
+    +---+---+
+    | I | A |
+    +---+---+
+    """
+    def split(self,item):
+        lA = self.l-item.l
+        wA = item.w
+        
+        lB = self.l
+        wB = self.w-item.w
+
+        item.location = self
+               
+        return [Block(lA,wA),Block(lB,wB)]
+
+
+class Segment(Region):
+    """
+    Segment is a region divided as follows:
+    +---+---+
+    | A |   |
+    +---+ B |
+    | I |   |
+    +---+---+
+    """
+    def split(self,item):
+        lA = self.l-item.l
+        wA = item.w
+        
+        lB = self.l
+        wB = self.w-item.w
+
+        item.location = self
+        
+        return [Block(lA,wA),Block(lB,wB)]
+
